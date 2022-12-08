eceeb90b0bcb8284d656d066bfca399a52631ef7
Saglara <nohaevass@yandex.ru>
2022-12-08 21:06:52 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,2 +1,6 @@
 # Напишите программу, удаляющую из текста все слова, содержащие ""абв""
 
+input_str = input("введите строку: ")
+list = (lambda x: x.split('абв'))(input_str)
+res=''.join(list)
+print (res)
