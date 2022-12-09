040ff918c10fd869fd628403c612529529a8914d
Saglara <nohaevass@yandex.ru>
2022-12-09 20:38:41 +0300

    Update 4.py

--- 4.py
+++ 4.py
@@ -7,28 +7,24 @@ data2 = open('text2.txt','a',encoding='UTF-8')
 for s in data1.readlines():
     s=list(s)
     print(s)
+    print()
     s1=[]
-    for i in range(1,len(s)):
-        count=0
-        while s[i]==s[i-1]:
-            count+=1
-            print(count)
-        s1.append(count)
-        s1.append(s[i])
-print(s1)
+    # for i in range(1,len(s)):
+    #     count=0
+    #     while s[i]==s[i-1]:
+    #         count+=1
+    #         print(count)
+    #     s1.append(count)
+    #     s1.append(s[i])
+# print(s1)
 data1.close()
 data2.write(s1)
 data2.close()
 
-
-
-
 # Задача 2. В текстовом файле посчитать количество строк, а также для каждой отдельной строки определить количество 
 # в ней символов и слов.
 
-
 # data = open('file.txt', 'r', encoding='UTF-8')
-
 # for s in data.readlines():
 #     print(s, end='')
 #     print(f'Количество символов = {len(s)}')
